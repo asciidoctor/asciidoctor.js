f567f81826ce8258e5730c814d73f773cd6657d0
Anthonny Quérouil <anthonny.querouil@gmail.com>
2014-05-27 00:39:19 +0200

    Add include support using XMLHttpRequest wrapper

--- dist/npm/asciidoctor-core.js
+++ dist/npm/asciidoctor-core.js
@@ -1,6 +1,11 @@
 module.exports = function(opalParam) {
   var Opal = opalParam || require('opal-npm-wrapper').Opal;
 
+  //Use xmlhttprequest to emulate the browser XMLHttpRequest object.
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  //Define overrideMimeType, not define by default in wrapper
+  XMLHttpRequest.prototype.overrideMimeType = function() {};
+
   return {
     Opal: Opal,
     Asciidoctor: function(loadExtensions) {
