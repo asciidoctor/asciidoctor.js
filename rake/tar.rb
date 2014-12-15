96a86b6c0ce32dd46c6e3249cc72d454228c545c
Guillaume Grossetie <ggrossetie@gmail.com>
2014-12-15 10:36:37 +0100

    Removes unused methods

--- rake/tar.rb
+++ rake/tar.rb
@@ -5,46 +5,6 @@ require 'fileutils'
 
 module Util
   module Tar
-    # Creates a tar file in memory recursively
-    # from the given path.
-    #
-    # Returns a StringIO whose underlying String
-    # is the contents of the tar file.
-    def tar(path)
-      tarfile = StringIO.new("")
-      Gem::Package::TarWriter.new(tarfile) do |tar|
-        Dir[File.join(path, "**/*")].each do |file|
-          mode = File.stat(file).mode
-          relative_file = file.sub /^#{Regexp::escape path}\/?/, ''
-
-          if File.directory?(file)
-            tar.mkdir relative_file, mode
-          else
-            tar.add_file relative_file, mode do |tf|
-              File.open(file, "rb") { |f| tf.write f.read }
-            end
-          end
-        end
-      end
-
-      tarfile.rewind
-      tarfile
-    end
-
-    # gzips the underlying string in the given StringIO,
-    # returning a new StringIO representing the 
-    # compressed file.
-    def gzip(tarfile)
-      gz = StringIO.new("")
-      z = Zlib::GzipWriter.new(gz)
-      z.write tarfile.string
-      z.close # this is necessary!
-
-      # z was closed to write the gzip footer, so
-      # now we need a new StringIO
-      StringIO.new gz.string
-    end
-
     # un-gzips the given IO, returning the
     # decompressed version as a StringIO
     def ungzip(tarfile)
