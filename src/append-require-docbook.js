454a5e78ea1a6b12eee05b7a6cd0467ef6190034
Guillaume Grossetie <ggrossetie@gmail.com>
2015-10-04 15:12:12 +0200

    Fix require module

--- src/append-require-docbook.js
+++ src/append-require-docbook.js
@@ -1,4 +1,4 @@
 // By default automatically require Asciidoctor Docbook module unless variable AsciidoctorConfiguration.autoRequireModule is set to 'false'
 if (typeof AsciidoctorConfiguration === 'undefined' || typeof AsciidoctorConfiguration.autoRequireModule === 'undefined' || Boolean(AsciidoctorConfiguration.autoRequireModule)) {
-  Opal.require('asciidoctor-docbook');
+  Opal.require('asciidoctor/docbook');
 }
--- src/append-require-extensions.js
+++ src/append-require-extensions.js
@@ -1,4 +1,4 @@
 // By default automatically require Asciidoctor Extensions module unless variable AsciidoctorConfiguration.autoRequireModule is set to 'false'
 if (typeof AsciidoctorConfiguration === 'undefined' || typeof AsciidoctorConfiguration.autoRequireModule === 'undefined' || Boolean(AsciidoctorConfiguration.autoRequireModule)) {
-  Opal.require('asciidoctor-extensions');
+  Opal.require('asciidoctor/extensions');
 }
