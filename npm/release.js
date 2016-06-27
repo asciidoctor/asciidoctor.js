243a9f6c8bbf1f720356bc3ce58de96553ca2aea
Guillaume Grossetie <ggrossetie@gmail.com>
2016-06-27 13:11:22 +0200

    Export environment variables in a cross-compatible way

--- npm/release.js
+++ npm/release.js
@@ -1,7 +1,11 @@
 var Log = require('./log.js');
+var Arg = require('./arg.js');
 var Builder = require('./builder.js');
 var builder = new Builder();
 var log = new Log();
+var arg = new Arg();
+
+arg.exportEnvironmentVariables();
 
 var args = process.argv.slice(2);
 var releaseVersion = args[0];
