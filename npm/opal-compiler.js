19116445ba65d0dcdae2504da3a8085bc732a5b7
Guillaume Grossetie <ggrossetie@gmail.com>
2016-08-16 19:23:38 +0200

    Remove duplicated log

--- npm/opal-compiler.js
+++ npm/opal-compiler.js
@@ -15,7 +15,6 @@ function OpalCompiler(config) {
 }
 
 OpalCompiler.prototype.compile = function(require, outputFile, includes) {
-  log.debug('compile ' + require);
   var builder = Builder.$new();
   builder.$append_paths('node_modules/opal-compiler/src/stdlib', 'lib', 'build/asciidoctor/lib');
   builder.compiler_options = Opal.hash({'dynamic_require_severity': this.dynamicRequireLevel});
