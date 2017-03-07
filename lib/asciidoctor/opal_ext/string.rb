2f993ee07412ffbdedf1ec5ee3160aa7f24755e3
Dan Allen <dan.j.allen@gmail.com>
2017-03-07 02:20:51 -0700

    make Asciidoctor.js compatible with Asciidoctor 1.5.6
    - define a limited implementation of String#unpack
    - update Kernel#freeze to return self
    - rename String#limit to String#limit_bytesize and add alias

--- lib/asciidoctor/opal_ext/string.rb
+++ lib/asciidoctor/opal_ext/string.rb
@@ -1,8 +1,17 @@
 class String
   # Safely truncate the string to the specified number of bytes.
-  def limit size
+  def limit_bytesize size
     return self.to_s unless size < bytes.length
     result = byteslice 0, size
     result.to_s
-  end unless method_defined? :limit
+  end unless method_defined? :limit_bytesize
+  alias :limit :limit_bytesize unless method_defined? :limit
+
+  def unpack format
+    if format == 'C3'
+      self[0, 3].bytes.select.with_index {|_, i| i.even? }
+    else
+      raise %(undefined method `unpack' for #{inspect}#{self.class})
+    end
+  end unless method_defined? :unpack
 end
