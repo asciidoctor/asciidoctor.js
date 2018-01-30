3c4ffc79cc4a2885cb6d61da95ad613b38069f62
Guillaume Grossetie <ggrossetie@gmail.com>
2018-01-30 14:20:22 +0100

    ✨ Refactor benchmark scripts
    - Replace PhantomJS by Chrome using https://github.com/GoogleChrome/puppeteer[Puppeteer]
    - Use a dedicated script per environment
    - Nashorn script (jjs) can use environment variable with the `-scripting` option
    - Define the base directory to be able to resolve include in each environments
    - Rename jjs runner to nashorn

---
+++ benchmark/node.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const verbose = process.env.VERBOSE;
+const include = process.env.INCLUDE;
+const runs = process.env.RUNS || 4;
+
+const start = new Date().getTime()
+const Asciidoctor = require('../asciidoctor.js');
+console.log(`Load scripts: ${((new Date().getTime() - start) / 1000.0)}s`);
+
+const baseDir = __dirname;
+const options = {
+  safe: 'safe',
+  base_dir: baseDir,
+  doctype: 'article',
+  header_footer: true,
+  attributes: 'linkcss copycss! toc! numbered! icons! compat-mode'
+};
+const asciidoctor = Asciidoctor({'runtime': {'platform': 'node'}});
+
+let content;
+if (include) {
+  content = 'include::userguide.adoc[]';
+} else {
+  content = fs.readFileSync(`${baseDir}/userguide.adoc`, 'utf-8');
+}
+let html;
+for (var i = 1; i <= runs; i++) {
+  let start = new Date().getTime();
+  html = asciidoctor.convert(content, options);
+  let duration = new Date().getTime() - start;
+  console.log(`Run #${i}: ${(duration / 1000.0)}s`);
+}
+
+if (verbose) {
+  console.log(html);
+}
