10c1ca2e586037ec815589ea703a2d66be792ebc
Guillaume Grossetie <g.grossetie@gmail.com>
2018-03-19 10:02:21 +0100

    🎉 Override the include path resolution in a browser environment (#441)
    * Override the include path resolution in a browser environment
    * Document how include directive is working on the browser

--- lib/asciidoctor/js/postscript.rb
+++ lib/asciidoctor/js/postscript.rb
@@ -1,3 +1,7 @@
 require 'asciidoctor/converter/composite'
 require 'asciidoctor/converter/html5'
 require 'asciidoctor/extensions'
+
+if JAVASCRIPT_IO_MODULE == 'xmlhttprequest'
+  require 'asciidoctor/js/opal_ext/browser/reader'
+end
