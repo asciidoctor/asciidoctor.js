62d4d2916f792f789e2d3bc79429f0d443707d6b
Guillaume Grossetie <g.grossetie@gmail.com>
2018-03-27 00:30:40 +0200

    ✨ Allow console.log in development (#466)
    * Allow console.log in development
    * Run the lint task at the end

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'browser': true
+  },
+  'rules': {
+    'space-before-function-paren': ['error', 'always'],
+    'no-cond-assign': 'off',
+    'no-undef': 'off',
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    // allow console in development
+    'no-console': process.env.NODE_ENV === 'development' ? 'off' : 'error'
+  }
+};
