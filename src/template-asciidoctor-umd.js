9d9c46e0f4aad1539d3ffb62a268e122e8fc3e4f
Guillaume Grossetie <g.grossetie@gmail.com>
2018-11-15 17:13:12 +0100

    🐛 Resolves #273 #556, Upgrade to Opal runtime/compiler 1.0.11 (#594)
    - include trailing slash on Windows path
    - preserve built-in objects methods
    - add tests

--- src/template-asciidoctor-umd.js
+++ src/template-asciidoctor-umd.js
@@ -1,15 +1,47 @@
-var functionCall = Function.call;
-
+/* eslint-disable indent */
 if (typeof Opal === 'undefined' && typeof module === 'object' && module.exports) {
   Opal = require('opal-runtime').Opal;
 }
 
 if (typeof Opal === 'undefined') {
-//{{opalCode}}
-}
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects#Fundamental_objects
+  var fundamentalObjects = [
+    Function,
+    Boolean,
+    Error,
+    Number,
+    Date,
+    String,
+    RegExp,
+    Array
+  ];
+  var backup = {};
+  for (var index in fundamentalObjects) {
+    var fundamentalObject = fundamentalObjects[index];
+    backup[fundamentalObject.name] = {
+      call: fundamentalObject.call,
+      apply: fundamentalObject.apply,
+      bind: fundamentalObject.bind
+    };
+  }
 
-// save and restore Function.call until https://github.com/opal/opal/issues/1846 is fixed
-Function.call = functionCall;
+//{{opalCode}}
+
+  // restore Function methods (see https://github.com/opal/opal/issues/1846)
+  for (var index in fundamentalObjects) {
+    var fundamentalObject = fundamentalObjects[index];
+    var name = fundamentalObject.name;
+    if (typeof fundamentalObject.call !== 'function') {
+      fundamentalObject.call = backup[name].call;
+    }
+    if (typeof fundamentalObject.apply !== 'function') {
+      fundamentalObject.apply = backup[name].apply;
+    }
+    if (typeof fundamentalObject.bind !== 'function') {
+      fundamentalObject.bind = backup[name].bind;
+    }
+  }
+}
 
 // UMD Module
 (function (root, factory) {
