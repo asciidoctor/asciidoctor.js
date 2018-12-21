9fe549535527b35eab7bf8deeaf928f0504d980b
Guillaume Grossetie <g.grossetie@gmail.com>
2018-12-21 15:25:33 +0100

    🔥 Remove Nashorn (and Rhino) support (#624)

--- npm/benchmark.js
+++ npm/benchmark.js
@@ -11,7 +11,7 @@ const BuilderModule = require('./module/builder')
 const args = process.argv.slice(2)
 const runner = args[0]
 
-const runners = ['node', 'chrome', 'nashorn']
+const runners = ['node', 'chrome']
 if (!runners.includes(runner)) {
   log.error(`Runner must be one of: ${runners.join(', ')}. 'npm run benchmark [${runners.join('|')}]'`)
   process.exit(9)
@@ -24,8 +24,8 @@ const getContentFromAsciiDocRepo = (source, target) => {
 const builder = new BuilderModule()
 builder.build()
   .then(() => {
-    bfs.mkdirsSync(builder.benchmarkBuildDir);
-    ['node', 'nashorn', 'chrome'].forEach(runner => bfs.copyToDirSync(`benchmark/${runner}.js`, builder.benchmarkBuildDir))
+    bfs.mkdirsSync(builder.benchmarkBuildDir)
+    ['node', 'chrome'].forEach(runner => bfs.copyToDirSync(`benchmark/${runner}.js`, builder.benchmarkBuildDir))
     log.task('download sample data from AsciiDoc repository')
     return Promise.all([
       getContentFromAsciiDocRepo('asciidoc.txt', 'build/benchmark/userguide.adoc'),
@@ -36,8 +36,6 @@ builder.build()
     log.task('run benchmark')
     if (runner === 'chrome') {
       execModule.execSync('node ' + path.join(builder.benchmarkBuildDir, 'chrome.js'))
-    } else if (runner === 'nashorn') {
-      execModule.execSync('jjs ' + path.join(builder.benchmarkBuildDir, 'nashorn.js'))
     } else if (runner === 'node') {
       execModule.execSync('node ' + path.join(builder.benchmarkBuildDir, 'node.js'))
     } else {
