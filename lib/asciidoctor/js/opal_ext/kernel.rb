cabbc7e9ffcf4c33bded9eb89c3fc7f2d3fcb200
Guillaume Grossetie <ggrossetie@gmail.com>
2018-12-29 16:38:37 +0100

    Remove superfluous module override
    - Use Opal.config.unsupported_features_severity to suppress warning messages
    - Remove ThreadSafe module (not required anymore by Asciidoctor core)

--- lib/asciidoctor/js/opal_ext/kernel.rb
+++
@@ -1,8 +0,0 @@
-module Kernel
-  # Redefine Kernel#freeze to suppress "not supported" warning message from Opal.
-  # TODO Remove once https://github.com/opal/opal/issues/1253 is resolved.
-  # NOTE Opal should define freeze on Object, not Kernel.
-  def freeze
-    self
-  end
-end
