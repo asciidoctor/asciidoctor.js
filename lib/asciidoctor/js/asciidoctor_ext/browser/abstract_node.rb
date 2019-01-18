d010c9725a06b36deae04511c1f21a1b0100411c
Guillaume Grossetie <g.grossetie@gmail.com>
2019-01-18 11:04:12 +0100

    ✨ Resolves #640, Implement generate_data_uri_from_uri in a Browser environment (#642)

---
+++ lib/asciidoctor/js/asciidoctor_ext/browser/abstract_node.rb
@@ -0,0 +1,61 @@
+module Asciidoctor
+class AbstractNode
+  def generate_data_uri_from_uri image_uri, cache_uri = false
+    %x{
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', #{image_uri}, false)
+      // the response type cannot be changed for synchronous requests made from a document
+      // xhr.responseType = 'arraybuffer'
+      xhr.overrideMimeType('text/plain; charset=x-user-defined')
+
+      var contentType = ''
+      var b64encoded = ''
+      var status = -1
+
+      try {
+        xhr.addEventListener('load', function() {
+          status = this.status
+          // status is 0 for local file mode (i.e., file://)
+          if (status === 0 || status === 200) {
+            var binary = ''
+            var rawText = this.responseText;
+            for (var i = 0, len = rawText.length; i < len; ++i) {
+              var c = rawText.charCodeAt(i)
+              var byteCode = c & 0xff // byte at offset i
+              binary += String.fromCharCode(byteCode)
+            }
+            b64encoded = btoa(binary)
+            contentType = this.getResponseHeader('content-type')
+          }
+        })
+        xhr.send(null)
+        // try to detect the MIME Type from the file extension
+        if (!contentType) {
+          if (#{image_uri}.endsWith('.jpeg') || #{image_uri}.endsWith('.jpg') || #{image_uri}.endsWith('.jpe')) {
+            contentType = 'image/jpg'
+          } else if (#{image_uri}.endsWith('.png')) {
+            contentType = 'image/png'
+          } else if (#{image_uri}.endsWith('.svg')) {
+            contentType = 'image/svg+xml'
+          } else if (#{image_uri}.endsWith('.bmp')) {
+            contentType = 'image/bmp'
+          } else if (#{image_uri}.endsWith('.tif') || #{image_uri}.endsWith('.tiff')) {
+            contentType = 'image/tiff'
+          }
+        }
+      }
+      catch (e) {
+        // something bad happened!
+        status = 0
+      }
+
+      // assume that no data in local file mode means it doesn't exist
+      if (status === 404 || (status === 0 && (!b64encoded || !contentType))) {
+        self.$logger().$warn('could not retrieve image data from URI: ' + #{image_uri})
+        return #{image_uri}
+      }
+      return 'data:' + contentType + ';base64,' + b64encoded
+    }
+  end
+end
+end
--- lib/asciidoctor/js/postscript.rb
+++ lib/asciidoctor/js/postscript.rb
@@ -4,5 +4,6 @@ require 'asciidoctor/extensions'
 
 if JAVASCRIPT_IO_MODULE == 'xmlhttprequest'
   require 'asciidoctor/js/asciidoctor_ext/browser/reader'
+  require 'asciidoctor/js/asciidoctor_ext/browser/abstract_node'
 end
 require 'asciidoctor/js/asciidoctor_ext/stylesheet'
