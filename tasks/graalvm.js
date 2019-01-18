b4f7f7e8549c9a7b6fae3c6d415f837762ce65a3
Guillaume Grossetie <ggrossetie@gmail.com>
2019-01-18 13:49:53 +0100

    👷 Execute the command from npm scripts
    - Output logs to stdout
    - Fail the build if the command exit with a non-zero code

--- tasks/graalvm.js
+++ tasks/graalvm.js
@@ -2,10 +2,8 @@
 
 const GraalVMModule = require('./module/graalvm')
 const graalvmModule = new GraalVMModule()
-const execModule = require('./module/exec')
 
 ;(async () => {
   await graalvmModule.get()
-  execModule.execSync('./build/graalvm/bin/node spec/graalvm/run.js')
   GraalVMModule.run()
 })()
