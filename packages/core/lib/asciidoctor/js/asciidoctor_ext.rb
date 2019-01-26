ee9c17031d79c882f5248d5fbd3a31c9b9c23cb4
Guillaume Grossetie <ggrossetie@gmail.com>
2019-01-26 14:06:10 +0100

    resolves #661, issue a debug when the syntax highlighter is not found

--- packages/core/lib/asciidoctor/js/asciidoctor_ext.rb
+++ packages/core/lib/asciidoctor/js/asciidoctor_ext.rb
@@ -2,6 +2,7 @@ require 'asciidoctor/js/asciidoctor_ext/stylesheet'
 require 'asciidoctor/js/asciidoctor_ext/document'
 require 'asciidoctor/js/asciidoctor_ext/substitutors'
 require 'asciidoctor/js/asciidoctor_ext/parser'
+require 'asciidoctor/js/asciidoctor_ext/syntax_highlighter'
 
 %x(
 // Load specific runtime
