5b90bdb87be17b555ed592e9c0dbf250ed6da63a
Guillaume Grossetie <g.grossetie@gmail.com>
2019-02-09 20:39:43 +0100

    ✨ Resolves #653, provide an implementation for Kernel#sprintf (#663)
    Allow to remove Kernel#sprintf (alias #format).

---
+++ lib/asciidoctor/js/asciidoctor_ext/substitutors.rb
@@ -0,0 +1,8 @@
+module Asciidoctor
+module Substitutors
+  # Avoid using Kernel#sprintf for performance and bundle size reasons
+  def sub_placeholder format_string, replacement
+    `format_string.replace('%s', replacement)`
+  end
+end
+end
