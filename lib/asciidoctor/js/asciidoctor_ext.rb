c49bf7183f37b0e94a488f06552b46fff01d1a71
Guillaume Grossetie <g.grossetie@gmail.com>
2019-02-28 18:00:52 +0100

    ⚡️ resolves #683 provide a JavaScript implementation for Parser#uniform? (#685)

--- lib/asciidoctor/js/asciidoctor_ext.rb
+++ lib/asciidoctor/js/asciidoctor_ext.rb
@@ -1,5 +1,6 @@
 require 'asciidoctor/js/asciidoctor_ext/stylesheet'
 require 'asciidoctor/js/asciidoctor_ext/substitutors'
+require 'asciidoctor/js/asciidoctor_ext/parser'
 
 %x(
 // Load specific runtime
---
+++ lib/asciidoctor/js/asciidoctor_ext/parser.rb
@@ -0,0 +1,13 @@
+module Asciidoctor
+class Parser
+  if `String.prototype.repeat`
+    def self.uniform? str, chr, len
+      `chr.repeat(len) === str`
+    end
+  else
+    def self.uniform? str, chr, len
+      `Array.apply(null, { length: len }).map(function () { return chr }).join('') === str`
+    end
+  end
+end
+end
