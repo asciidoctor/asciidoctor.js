fd38aa310ca1f64b7390481d96935cd9779ec6cc
Guillaume Grossetie <ggrossetie@gmail.com>
2019-02-28 18:14:51 +0100

    Add comments to explain why we override Logger

--- lib/asciidoctor/js/postscript.rb
+++ lib/asciidoctor/js/postscript.rb
@@ -1,6 +1,6 @@
 require 'asciidoctor/converter/composite'
 require 'asciidoctor/converter/html5'
 require 'asciidoctor/extensions'
-require 'asciidoctor/js/opal_ext/logger'
 
 require 'asciidoctor/js/asciidoctor_ext'
+require 'asciidoctor/js/opal_ext/logger' # override the built-in Logger
