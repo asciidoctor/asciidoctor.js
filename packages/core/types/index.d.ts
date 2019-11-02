9e5b35548263c5ae13bff6100501e601d052fd9c
Guillaume Grossetie <ggrossetie@gmail.com>
2019-11-02 23:57:59 +0100

    resolves #752 adds typescript declaration file (#756)
    * declaration file added
    * added types to package.json
    * move types definition to core package
    * added types tests

---
+++ packages/core/types/index.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="node" />
+// TypeScript Version: 2.2
+export interface AsciidoctorRuntime {
+  ioModule: string | 'node';
+  platform: string | 'node';
+  engine: string | 'v8';
+  framework: string;
+}
+
+export interface AsciidoctorDocument {
+  getTitle(): string;
+  setTitle(title: string): void;
+}
+
+export class Asciidoctor {
+  getVersion(): string;
+  getCoreVersion(): string;
+  getRuntime(): AsciidoctorRuntime;
+  convert(input: string | Buffer, options?: any): string | AsciidoctorDocument;
+  convertFile(filename: string, options?: any): string | AsciidoctorDocument;
+  load(input: string | Buffer, options?: any): AsciidoctorDocument;
+  loadFile(filename: string, options?: any): AsciidoctorDocument;
+  toString(): string;
+}
+
+export default function asciidoctor(): Asciidoctor;
