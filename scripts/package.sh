6385c0d93a5d9e53cab95e019646f5e01af691f8
Guillaume Grossetie <ggrossetie@gmail.com>
2020-04-16 13:34:48 +0200

    📦 resolves #767 produce and publish binaries for the asciidoctor package (#929)

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-# Package a distribution as a zip and tar.gz archive
 
 set -e
 
+# Package asciidoctor/core distribution as a zip and tar.gz archive
 cd "$(dirname "$0")"
-
 cd ../packages/core
 npm run dist
 mkdir bin
 cd dist/
 zip -r ../bin/asciidoctor.js.dist.zip .
 tar -zcvf ../bin/asciidoctor.js.dist.tar.gz .
+
+# Produce macOS, Linux and Windows binaries from asciidoctor/cli
+cd "$(dirname "$0")"
+cd ../packages/asciidoctor
+npm run dist
