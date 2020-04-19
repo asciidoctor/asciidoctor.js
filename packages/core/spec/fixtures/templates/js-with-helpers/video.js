1ca694919a49a4a6e12f3cc41c07645b28b92efc
Guillaume Grossetie <ggrossetie@gmail.com>
2020-04-19 15:51:56 +0200

    Finish the doccumentation about template converter

--- packages/core/spec/fixtures/templates/js-with-helpers/video.js
+++ packages/core/spec/fixtures/templates/js-with-helpers/video.js
@@ -23,6 +23,5 @@ module.exports = function ({ node, _, helpers }) {
   const src = `${helpers.getAssetUriScheme(document)}//www.youtube.com/embed/${target}?${params.join('&amp;')}`
   return `<figure class="video ${node.getRole()}"${node.getId() ? ` id="${node.getId()}"` : ''}>
   <iframe src="${src}" width="${helpers.getLocalAttribute(node, 'width')}" height="${helpers.getLocalAttribute(node, 'height')}" frameborder="0" allowfullscreen="${node.isOption('nofullscreen') ? '' : 'true'}" data-rewind="${rew || ''}" data-volume="${vol || ''}"/>
-</iframe>
 </figure>`
 }
