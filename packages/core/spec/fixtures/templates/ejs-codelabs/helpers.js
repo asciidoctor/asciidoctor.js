2db332e4b83bf034881208d58a8c73004ba00c85
Henriette Einstein <45788168+henriette-einstein@users.noreply.github.com>
2020-04-23 22:06:27 +0200

    ✅ Added EJS Codelab templates (#941)

---
+++ packages/core/spec/fixtures/templates/ejs-codelabs/helpers.js
@@ -0,0 +1,5 @@
+module.exports.attributeIfSet = (attName, attValue) => {
+  if (attName && attValue !== undefined && attValue.trim().length > 0) {
+    return attName.trim() + '="' + attValue.trim() + '"'
+  }
+}
