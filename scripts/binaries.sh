3f68f41baf6f82ebaabc045dd6b51be19d3e87ef
Guillaume Grossetie <ggrossetie@gmail.com>
2020-04-28 10:10:19 +0200

    resolves #951 bundle templates engines into the binaries

--- scripts/binaries.sh
+++ scripts/binaries.sh
@@ -8,5 +8,6 @@ cd "$SCRIPT_PATH"
 cd ../packages/asciidoctor
 
 # @asciidoctor/core must be published to npmjs before we can install the dependencies
+npm i --save ejs@3.1.2 handlebars@4.7.6 nunjucks@3.2.1 pug@2.0.4
 npm i
 npm run dist
