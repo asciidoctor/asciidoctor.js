9f90977f8bef98b6181420b05b0ae32853699441
Guillaume Grossetie <ggrossetie@gmail.com>
2020-05-18 18:35:35 +0200

    Use export default

--- packages/core/src/template-asciidoctor-browser.js
+++ packages/core/src/template-asciidoctor-browser.js
@@ -1,22 +1,7 @@
 //{{opalCode}}
 
 // UMD Module
-(function (root, factory) {
-  if (typeof module === 'object' && module.exports) {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
-    module.exports = factory
-  } else if (typeof define === 'function' && define.amd) {
-    // AMD. Register a named module.
-    define('asciidoctor', ['module'], function (module) {
-      return factory(module.config())
-    })
-  } else {
-    // Browser globals (root is window)
-    root.Asciidoctor = factory
-  }
-}(this, function (moduleConfig) {
+export default function (moduleConfig) {
 //{{asciidoctorCode}}
 
 //{{asciidoctorAPI}}
@@ -33,4 +18,4 @@
     return ASCIIDOCTOR_JS_VERSION
   }
   return Opal.Asciidoctor
-}))
+}
