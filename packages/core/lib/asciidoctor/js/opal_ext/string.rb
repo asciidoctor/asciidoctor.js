5557b1b727d5b72478a2e95da82bc0f8aa2695aa
Guillaume Grossetie <ggrossetie@gmail.com>
2021-04-13 22:36:09 +0200

    🐛 Implement unpack 'C3' with charCodeAt (#1265)

--- packages/core/lib/asciidoctor/js/opal_ext/string.rb
+++ packages/core/lib/asciidoctor/js/opal_ext/string.rb
@@ -11,7 +11,22 @@ class String
 
   def unpack format
     if format == 'C3'
-      self[0, 3].bytes.select.with_index {|_, i| i.even? }
+      # this implementation is good enough to check byte-order mark (BOM) at the beginning of an AsciiDoc file.
+      # please note that this method will return wrong results on multi-bytes characters but since byte-order mark are single byte it's fine!
+      # BOM_BYTES_UTF_8    = [ 239, 187, 191 ]
+      # BOM_BYTES_UTF_16LE = [ 255, 254 ]
+      # BOM_BYTES_UTF_16BE = [ 254, 255 ]
+      %x{
+        var bytes = []
+        for (var i=0; i < 3; i++) {
+          if (i < self.length) {
+            bytes.push(self.charCodeAt(i))
+          } else {
+            bytes.push(nil)
+          }
+        }
+        return bytes
+      }
     else
       _original_unpack format
     end
