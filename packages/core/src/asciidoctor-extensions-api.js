aaf0e1092a40314490f720f1126c083cbe507463
Guillaume Grossetie <g.grossetie@gmail.com>
2022-01-20 13:43:44 +0100

    Replace spread and Array.from by Array.slice

--- packages/core/src/asciidoctor-extensions-api.js
+++ packages/core/src/asciidoctor-extensions-api.js
@@ -1314,7 +1314,7 @@ ConverterFactory.register = function (converter, backends) {
   }
   var bridgeSendMethodToInstance = function (obj, instance) {
     bridgeMethodToInstance(obj, instance, '$send', 'send', function (symbol) {
-      var [, ...args] = Array.from(arguments)
+      var args = Array.prototype.slice.call(arguments, 1)
       var func = instance['$' + symbol]
       if (func) {
         return func.apply(instance, args)
