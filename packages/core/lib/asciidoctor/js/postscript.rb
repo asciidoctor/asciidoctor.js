b39bb7ea049909909d113d3a7b0680bed465e05f
Guillaume Grossetie <ggrossetie@gmail.com>
2024-03-17 18:40:01 +0100

    Fix compatibility issues

--- packages/core/lib/asciidoctor/js/postscript.rb
+++ packages/core/lib/asciidoctor/js/postscript.rb
@@ -4,3 +4,5 @@ require 'asciidoctor/extensions'
 
 require 'asciidoctor/js/asciidoctor_ext'
 require 'asciidoctor/js/opal_ext/logger' # override the built-in Logger
+
+Asciidoctor::InlineLinkRx = %r((^|link:|#{Asciidoctor::CG_BLANK}|\\?&lt;(?=\\?(?:https?|file|ftp|irc)(:))|[>\(\)\[\];"'])(\\?(?:https?|file|ftp|irc)://)(?:([^\s\[\]]+)\[(|#{Asciidoctor::CC_ALL}*?[^\\])\]|(?!\2)([^\s]*?)&gt;|([^\s\[\]<]*([^\s,.?!\[\]<\)]))))
