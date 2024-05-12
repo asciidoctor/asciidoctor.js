6174f933f607ac5aef71e25b5d2b8a437c0e44e6
Dan Allen <dan.j.allen@gmail.com>
2024-05-12 12:48:17 -0600

    remove outdated rx patch

--- packages/core/lib/asciidoctor/js/postscript.rb
+++ packages/core/lib/asciidoctor/js/postscript.rb
@@ -4,5 +4,3 @@ require 'asciidoctor/extensions'
 
 require 'asciidoctor/js/asciidoctor_ext'
 require 'asciidoctor/js/opal_ext/logger' # override the built-in Logger
-
-Asciidoctor::InlineLinkRx = %r((^|link:|#{Asciidoctor::CG_BLANK}|\\?&lt;(?=\\?(?:https?|file|ftp|irc)(:))|[>\(\)\[\];"'])(\\?(?:https?|file|ftp|irc)://)(?:([^\s\[\]]+)\[(|#{Asciidoctor::CC_ALL}*?[^\\])\]|(?!\2)([^\s]*?)&gt;|([^\s\[\]<]*([^\s,.?!\[\]<\)]))))
