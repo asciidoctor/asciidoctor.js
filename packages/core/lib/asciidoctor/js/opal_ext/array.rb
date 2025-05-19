901e2fe3d60cb4350f25dd5baa5e553536a64c38
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2025-05-19 13:48:42 +0200

    chore: implement Array#pack m0 (alias for Base64.encode)
    Upstream change: https://github.com/asciidoctor/asciidoctor/commit/d3b48e28973ccec40dac8e84d382fa25aa4c2521

---
+++ packages/core/lib/asciidoctor/js/opal_ext/array.rb
@@ -0,0 +1,24 @@
+class Array
+  %x{
+    var encode;
+    encode = Opal.global.btoa || function (input) {
+      var buffer;
+      if (input instanceof Buffer) {
+        buffer = input;
+      } else {
+        buffer = Buffer.from(input.toString(), 'binary');
+      }
+      return buffer.toString('base64');
+    };
+  }
+
+  alias :_original_pack :pack
+
+  def pack format
+    if format == 'm0'
+      `encode(#{self}.join(''))`
+    else
+      _original_pack format
+    end
+  end
+end
