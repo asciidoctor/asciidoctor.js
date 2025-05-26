35e3ab7925c7a17502f43f38533672caea922c5f
Guillaume Grossetie <ggrossetie@gmail.com>
2025-05-26 13:41:22 +0200

    chore: replace eslint by prettier (#1751)

--- spec/build/builder.spec.js
+++ spec/build/builder.spec.js
@@ -1,9 +1,4 @@
-const {
-  describe,
-  it,
-  beforeEach,
-  afterEach
-} = require('node:test')
+const { describe, it, beforeEach, afterEach } = require('node:test')
 const assert = require('node:assert')
 const childProcess = require('child_process')
 const execModule = require('../../tasks/module/exec')
@@ -22,11 +17,14 @@ describe('Build', function () {
 
   describe('Push release', function () {
     it('should be able to find the remote name', function () {
-      childProcessExecSyncStub.withArgs('git remote -v')
-        .returns('origin\tgit@github.com:Mogztter/asciidoctor.js.git\t(fetch)\n' +
-          'origin\tgit@github.com:Mogztter/asciidoctor.js.git\t(push)\n' +
-          'upstream\tgit@github.com:asciidoctor/asciidoctor.js.git\t(fetch)\n' +
-          'upstream\tgit@github.com:asciidoctor/asciidoctor.js.git\t(push)')
+      childProcessExecSyncStub
+        .withArgs('git remote -v')
+        .returns(
+          'origin\tgit@github.com:Mogztter/asciidoctor.js.git\t(fetch)\n' +
+            'origin\tgit@github.com:Mogztter/asciidoctor.js.git\t(push)\n' +
+            'upstream\tgit@github.com:asciidoctor/asciidoctor.js.git\t(fetch)\n' +
+            'upstream\tgit@github.com:asciidoctor/asciidoctor.js.git\t(push)'
+        )
       const releaseModule = require('../../tasks/module/release.js')
       const execSyncStub = sinon.stub(execModule, 'execSync')
       execSyncStub.returns('void')
@@ -39,9 +37,12 @@ describe('Build', function () {
     })
 
     it('should return false if the original repository is absent', function () {
-      childProcessExecSyncStub.withArgs('git remote -v')
-        .returns('origin\tgit@github.com:ldez/asciidoctor.js.git (fetch)\n' +
-          'origin\tgit@github.com:ldez/asciidoctor.js.git (push)')
+      childProcessExecSyncStub
+        .withArgs('git remote -v')
+        .returns(
+          'origin\tgit@github.com:ldez/asciidoctor.js.git (fetch)\n' +
+            'origin\tgit@github.com:ldez/asciidoctor.js.git (push)'
+        )
       const releaseModule = require('../../tasks/module/release.js')
       const execSyncStub = sinon.stub(execModule, 'execSync')
       execSyncStub.returns('void')
