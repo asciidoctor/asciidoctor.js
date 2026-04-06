a6f436e7842673b20affbbcc225589cf64a58802
Guillaume Grossetie <ggrossetie@gmail.com>
2026-04-06 14:29:39 +0200

    wip: fix block image tests failure

--- packages/native/test/helpers.js
+++ packages/native/test/helpers.js
@@ -51,10 +51,11 @@ export function countXpath (html, xpath) {
       .replace(/<(meta|link|br|hr|img|input|area|base|col|embed|param|source|track|wbr)([^>]*)>/gi, '<$1$2/>')
       .trim()
   } else {
-    // Fragment: always wrap in <root> so XML has a single root and XPath can address any number
-    // of top-level elements.  Absolute XPath paths (starting with / but not //) are rewritten
-    // to include the wrapper so that /*[@class="foo"] and (/*[@class="foo"])[1]/... still work.
-    xmlSrc = `<root>${html}</root>`
+    // Fragment: self-close void elements so xmldom can parse as XML, then wrap in <root>
+    // so XML has a single root and XPath can address any number of top-level elements.
+    // Absolute XPath paths (starting with / but not //) are rewritten to include the wrapper
+    // so that /*[@class="foo"] and (/*[@class="foo"])[1]/... still work.
+    xmlSrc = `<root>${html.replace(/<(meta|link|br|hr|img|input|area|base|col|embed|param|source|track|wbr)([^>]*)>/gi, '<$1$2/>')}</root>`
     xpath = xpath
       .replace(/^\/([^/])/, '/root/$1')
       .replace(/\(\/([^/])/g, '(/root/$1')
