b7071cbe01bc7006972c5ab5c7b2362be92606b1
Guillaume Grossetie <ggrossetie@gmail.com>
2026-04-20 19:55:50 +0200

    chore: generate a browser bundle

--- packages/native/rollup.config.js
+++ packages/native/rollup.config.js
@@ -13,19 +13,54 @@ function nodeAwaitImportToRequire () {
   }
 }
 
-/** @type {import('rollup').RollupOptions} */
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'build/node/index.cjs',
-    format: 'cjs',
-    exports: 'named',
-    inlineDynamicImports: true,
-    generatedCode: { constBindings: true },
-  },
-  external: (id) => id.startsWith('node:'),
-  plugins: [
-    nodeAwaitImportToRequire(),
-    json(),
-  ],
+// Replaces TemplateConverter (filesystem-based) with a browser stub that throws
+// a clear error if template_dirs is used in a browser environment.
+function browserTemplateConverterStub () {
+  const STUB_ID = '\0browser:template-converter'
+  return {
+    name: 'browser-template-converter-stub',
+    resolveId (id, importer) {
+      if (id === './converter/template.js' || id.endsWith('/converter/template.js')) {
+        return STUB_ID
+      }
+    },
+    load (id) {
+      if (id === STUB_ID) {
+        return 'export class TemplateConverter { static async create() { throw new Error("TemplateConverter is not supported in browser environments") } }'
+      }
+    },
+  }
 }
+
+/** @type {import('rollup').RollupOptions[]} */
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'build/node/index.cjs',
+      format: 'cjs',
+      exports: 'named',
+      inlineDynamicImports: true,
+      generatedCode: { constBindings: true },
+    },
+    external: (id) => id.startsWith('node:'),
+    plugins: [
+      nodeAwaitImportToRequire(),
+      json(),
+    ],
+  },
+  {
+    input: 'src/browser.js',
+    output: {
+      file: 'build/browser/index.js',
+      format: 'esm',
+      inlineDynamicImports: true,
+      generatedCode: { constBindings: true },
+    },
+    external: (id) => id.startsWith('node:'),
+    plugins: [
+      browserTemplateConverterStub(),
+      json(),
+    ],
+  },
+]
---
+++ packages/native/src/browser.js
@@ -0,0 +1,106 @@
+import packageJson from '../package.json' with { type: 'json' }
+import { load } from './load.js'
+import { LoggerManager, MemoryLogger, NullLogger } from './logging.js'
+import { SafeMode } from './constants.js'
+import { Timings } from './timings.js'
+import { Extensions } from './extensions.js'
+import { Converter } from './converter.js'
+import { Block } from './block.js'
+import { Section } from './section.js'
+import Html5Converter from './converter/html5.js'
+import { SyntaxHighlighter, SyntaxHighlighterBase } from './syntax_highlighter.js'
+
+const ASCIIDOCTOR_CORE_VERSION = '2.0.26'
+
+class Asciidoctor {
+  /**
+   * Get the version of Asciidoctor.js.
+   *
+   * @returns {string} - the version of Asciidoctor.js
+   */
+  getVersion () {
+    return packageJson.version
+  }
+
+  /**
+   * Get Asciidoctor core version number.
+   *
+   * @returns {string} - the version of Asciidoctor core (Ruby)
+   */
+  getCoreVersion () {
+    return ASCIIDOCTOR_CORE_VERSION
+  }
+
+  get LoggerManager () {
+    return LoggerManager
+  }
+
+  get MemoryLogger () {
+    return MemoryLogger
+  }
+
+  get NullLogger () {
+    return NullLogger
+  }
+
+  get SafeMode () {
+    return SafeMode
+  }
+
+  get Timings () {
+    return Timings
+  }
+
+  get Extensions () {
+    return Extensions
+  }
+
+  get ConverterFactory () {
+    return Converter
+  }
+
+  get Html5Converter () {
+    return Html5Converter
+  }
+
+  get Block () {
+    return Block
+  }
+
+  get Section () {
+    return Section
+  }
+
+  get SyntaxHighlighter () {
+    return SyntaxHighlighter
+  }
+
+  /**
+   * Parse the AsciiDoc source input into a Document.
+   *
+   * @param {string|string[]|Buffer} input - the AsciiDoc source as a String, String Array, or Buffer
+   * @param {Object} [options={}] - a plain object of options to control processing
+   * @returns {Promise<Document>} - the parsed Document
+   */
+  async load (input, options = {}) {
+    return load(input, options)
+  }
+
+  /**
+   * Parse the AsciiDoc source input and convert it to the specified backend format.
+   *
+   * @param {string|string[]|Buffer} input - the AsciiDoc source as a String, String Array, or Buffer
+   * @param {Object} [options={}] - a plain object of options to control processing
+   * @returns {Promise<string>} - the converted output as a String
+   */
+  async convert (input, options = {}) {
+    const doc = await load(input, options)
+    return await doc.convert()
+  }
+}
+
+export { SyntaxHighlighterBase }
+
+export default function () {
+  return new Asciidoctor()
+}
